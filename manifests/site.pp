b595039a9dec9548fc6e561f3dd9bc94c23f03d5
nalsree <snalubola@outlook.com>
2021-02-14 00:19:13 -0800

    nixj

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
-node default {
-    file {'/root/README':
-     ensure  => file,
-     content => 'This is nalsree test. On sunday(2/13/2021) ',
-     owner   => 'root',
-    }   
+node default { 
+} 
+node'master.puppet.vm' {
+    include role::master_server
 }
---
+++ site/role/manifests/app_server.pp
@@ -0,0 +1,5 @@
+class role::app_server{
+  include profile::web
+  include profil::base
+  include profile::app
+}
